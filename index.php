8181986cffa0105ed65a567289e4128c60727018
agnesvarga1 <agikolyok@gmail.com>
2024-02-27 21:42:47 +0100

    generate pwd

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php ?>
+<?php
+
+$pwdLength =$_GET['length'] ;
+$pwdLength =intval($pwdLength);
+
+function pwdGen($pwdLength){
+  $digits    = (range('0', '9'));
+  $lowercase = (range('a', 'z'));
+  $uppercase = (range('A', 'Z')); 
+  $special   = (str_split('!@#$%^&*_+=?/'));
+  $combined  = array_merge($digits, $lowercase, $uppercase, $special);
+  $pwdArr = [];
+  for($i=0;$i < $pwdLength;$i++){
+    $randomPosition = rand(0,count($combined)-1);
+    $singleC = $combined[$randomPosition];
+    $pwdArr[] = $singleC;      
+}
+$pwd = implode($pwdArr);
+return $pwd;
+
+}
+
+
+
+
+   
+ 
+
+
+   
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +40,21 @@
     <title>Pwd Generator</title>
 </head>
 <body class="p-5">
-<form class="container" action="index.php" method="POST">
-  <div class=" row">
+<form  action="index.php" method="GET">
+
+  <div class="mb-3">
     <label for="length" class="form-label">Lungezza password</label>
-    <input type="number" class="form-control " id="length" aria-describedby="emailHelp">
+    <input type="number" class="form-control " id="length" name="length" aria-describedby="emailHelp">
   </div>
+  <?php if(isset($pwdLength) && $pwdLength !== 0){ ?>
+    <div class="m-5 p-3">
+    <span>Generated password:</span>
+   <span><?php echo pwdGen($pwdLength) ?> </span>
+    </div>
   
+
+ <?php } ?>
+<!--   
     <span>Consenti ripetizioni uno o piu caratteri</span>
 
   <div class="form-check">
@@ -47,7 +88,7 @@
   <label class="form-check-label" for="nums">
     Numeri
   </label>
-</div>
+</div> -->
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </body>
